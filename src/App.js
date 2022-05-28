f0fade686b29cb862d763d1c2e74fc9771f159b9
serikzannavii <seriknavii@gmail.com>
2022-05-28 23:02:59 +0600

    feat: change for update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is React app for deploy in github pages
+          This is update for this site
         </p>
       </header>
     </div>
